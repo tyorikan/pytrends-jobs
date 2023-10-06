b4865ee6d778632562d65728380fe7390e28545c
Takayuki Yorikane <tyorikan@gmail.com>
2023-10-06 16:10:44 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # pytrends-jobs
+
+## Quickstart: ローカル環境で実行するまで
+
+1. Sheets API と Drive API の有効化  
+https://console.cloud.google.com/apis/library/sheets.googleapis.com  
+https://console.cloud.google.com/apis/library/drive.googleapis.com
+
+2. API 同意画面で権限追加  
+https://console.cloud.google.com/apis/credentials/consent  
+* 機密性の高いスコープ  
+Google スプレッドシートのすべてのスプレッドシートの参照、編集、作成、削除  
+* 制限付きのスコープ  
+Google ドライブのすべてのファイルの表示、編集、作成、削除
+
+3. サービスアカウントを作成し、client_secret.json をダウンロード（TODO: デフォルト認証情報取得する方法があるかも）  
+
+4. Drive にフォルダを作成し、作成したサービスアカウントが編集できるようアクセス権追加
+
+5. 環境変数 FOLDER_ID にフォルダ ID を指定し、実行
+```
+export FOLDER_ID=xxxxxxxx
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import datetime
+import gspread
+
+from pytrends.request import TrendReq
+from oauth2client.service_account import ServiceAccountCredentials
+
+# pytrendsを初期化
+pytrends = TrendReq(hl='ja-JP', tz=360)
+
+# トレンドを取得したいキーワードを設定
+keywords = ["Pixel8"]
+
+# Googleトレンドデータを取得
+pytrends.build_payload(keywords, timeframe='now 7-d')
+
+# 関連クエリを取得
+related_queries = pytrends.related_queries()
+top_queries = related_queries[keywords[0]]['top']
+rising_queries = related_queries[keywords[0]]['rising']
+
+# 表示
+print(top_queries)
+print(rising_queries)
+
+# 以下、Sheets への書き出し
+#-------------------------
+
+# Google Sheets APIの認証情報を設定
+scope = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+# ファイルを作成
+date_str = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+file_name = "demo_pytrends_data_" + date_str
+folder_id = os.getenv("FOLDER_ID", None)
+client.create(title=file_name, folder_id=folder_id)
+
+# Google Sheetsを開く
+top_sheet = client.open(file_name).add_worksheet(title="TOP", rows=0, cols=0)
+rising_sheet = client.open(file_name).add_worksheet(title="RISING", rows=0, cols=0)
+
+# Google Sheetsに出力
+for i, row in top_queries.iterrows():
+    top_sheet.append_row(row.tolist())
+for i, row in rising_queries.iterrows():
+    rising_sheet.append_row(row.tolist())
